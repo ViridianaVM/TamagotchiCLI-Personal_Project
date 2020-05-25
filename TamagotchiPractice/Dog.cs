bf87283b5020c6781a68d822988e92a09d6bcaf6
Viridiana Vazquez <viridiana.vzq@gmail.com>
2020-05-25 01:14:02 -0700

    4th commit: Tamagotchi info saved in a file

---
+++ TamagotchiPractice/Dog.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TamagotchiPractice
+{
+    [Serializable]
+    class Dog
+    {
+        public String Name { get; set; }
+        public String Size { get; set; }
+        private static Stopwatch m_stopwatch;
+        private DateTime m_dob;
+        public int m_totalKalories;
+        private Food chocolateMeal = new Food(Food.FoodKind.CHOCOLATE);
+        private Food cheeseMeal = new Food(Food.FoodKind.CHEESE);
+        private Food veggiesMeal = new Food(Food.FoodKind.VEGGIES);
+        private Food meatMeal = new Food(Food.FoodKind.MEAT);
+
+        public Dog(String name)
+        {
+            Name = name;
+            Size = "small";
+            m_stopwatch = new Stopwatch();  //Timer to Play
+            m_dob = DateTime.Now;           //For timer to play
+        }
+
+        public Dog(String name, String size)
+        {
+            Name = name;
+            Size = size;
+            m_stopwatch = new Stopwatch();
+            m_dob = DateTime.Now;
+        }
+
+
+        public int getAge()
+        {
+            DateTime currentDate = DateTime.Now.AddDays(15);   //Adding 15 days to date
+            TimeSpan total = currentDate - m_dob;
+            double totalDays = total.TotalDays;
+            return (int)Math.Round(totalDays);
+        }
+
+        public int getKalories()
+        {
+            return m_totalKalories;
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("Woof Woof");
+        }
+
+        /// <summary>
+        /// PLAYING FUNCTIONS
+        /// </summary>
+        public void Play()
+        {
+            if (m_totalKalories < 50)
+            {
+                Console.WriteLine("Sorry! I am not string enough to play right now! I need more energy or go to sleep.");
+            }
+            else
+            {
+                //Right now will rest just 50 kal. But it should rest the kalories according to the time playing
+                m_totalKalories -= 50;
+                m_stopwatch.Start();
+                Console.WriteLine(Name + " has started to play!");
+            }
+            
+        }
+
+        public void StopPlaying()
+        {
+            m_stopwatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = m_stopwatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("Time Playing " + elapsedTime);
+            m_stopwatch.Reset();
+            Console.WriteLine(Name + " just stoped playing.");
+        }
+
+
+        /// <summary>
+        /// EATING FUNCTIONS
+        /// </summary>
+
+        public void EatChocolate(Food chocolateMeal)
+        {
+            m_totalKalories = m_totalKalories + chocolateMeal.getCalories();
+            if (m_totalKalories < 350)
+            {
+                Console.WriteLine("Thanks for the chocolate but I am still hungry. I will need to eat more later!");
+            }
+            else
+            {
+                Console.WriteLine("Thanks for the chocolate! I am so full that I don't need to ear for the rest of the day");
+            }
+        }
+
+        public void EatCheese(Food cheeseMeal)
+        {
+            m_totalKalories = m_totalKalories + cheeseMeal.getCalories();
+            if (m_totalKalories < 350)
+            {
+                Console.WriteLine("Thanks for the cheese but I am still hungry. I will need to eat more later!");
+            }
+            else
+            {
+                Console.WriteLine("Thanks for the cheese! I am so full that I don't need to eat for the rest of the day");
+            }
+        }
+
+        public void EatVeggies(Food veggiesMeal)
+        {
+            m_totalKalories = m_totalKalories + veggiesMeal.getCalories();
+            if (m_totalKalories < 350)
+            {
+                Console.WriteLine("Thanks for the veggies but I am still hungry. I will need to eat more later!");
+            }
+            else
+            {
+                Console.WriteLine("Thanks for the veggies! I am so full that I don't need to eat for the rest of the day");
+            }
+        }
+
+        public void EatMeat(Food meatMeal)
+        {
+            m_totalKalories = m_totalKalories + meatMeal.getCalories();
+            if (m_totalKalories < 350)
+            {
+                Console.WriteLine("Thanks for the meat but I am still hungry. I will need to eat more later!");
+            }
+            else
+            {
+                Console.WriteLine("Thanks for the meat! I am so full that I don't need to eat for the rest of the day");
+            }
+        }
+
+
+        /// <summary>
+        /// SLEEPING FUNCTION
+        /// </summary>
+        /// 
+        public void Sleep()
+        {
+            Console.WriteLine("I'll go and rest. Thanks");
+            m_totalKalories += 50;
+        }
+
+
+        /// <summary>
+        /// WALKING FUNCTION
+        /// </summary>
+        /// 
+        public void Walk()
+        {
+            if (m_totalKalories < 50)
+            {
+                Console.WriteLine("Sorry. I don't have enough energy to walk. I need to eat or rest");
+            }
+            else
+            {
+                m_totalKalories -= 50;
+                Console.WriteLine("Woof! I am super excited to walk with you!");
+            }
+            
+        }
+
+
+        //CREAR UNA FUNCION QUE SE LLAME TOSTRING
+        //SE MANDA A LLAMAR CUANDO HAGA DISPLAYINFO PARA MOSTRAR EL ASCCII DEL PERRITO Y TODA SU INFO
+
+    }
+}
--- TamagotchiPractice/Program.cs
+++ TamagotchiPractice/Program.cs
@@ -3,13 +3,195 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Linq;
+using System.CodeDom.Compiler;
 
 namespace TamagotchiPractice
 {
     class Program
     {
+        static string userAction;  //To read action from UI
+        static int uAction;        //User action converted to int
+        private const string DATA_FILENAME = "TamagotchiInformation.dat";  //To save Tamagotchi info
+        static public Dog cachito = new Dog("Cachito");
         static void Main(string[] args)
         {
+
+            //Code above can be avoided if there is already a Tamagotchi initialized
+            LoadInfo();
+            cachito.Bark();
+            displayInfo();
+            displayMenu();
+       
+        }
+
+        static void displayMenu()
+        {
+            Console.WriteLine("Welcome Back! What do you want to do now?");
+            Console.WriteLine("1. Feed my Tamagotchi" + "\n" + "2. Play with my Tamagotchi" + "\n" + "3. Take my Tamagotchi to a walk" + "\n" + "4. Put my Tamagotchi to sleep" + "\n" + "5. Exit");
+            userAction = Console.ReadLine();
+            uAction = Convert.ToInt32(userAction);
+            Console.Clear();
+            actionToTake();
+        }
+
+        static void actionToTake()
+        {
+            switch (uAction)
+            {
+                case 1:  //Feed
+                    {
+                        string foodKind;
+                        int food;
+                        Console.WriteLine("What food will you choose to feed " + cachito.Name);
+                        Console.WriteLine("1. Veggies" + "\n" + "2. Cheese" + "\n" + "3. Chocolate" + "\n" + "4. Meat");
+                        foodKind = Console.ReadLine();
+                        food = Convert.ToInt32(foodKind);
+                        Console.Clear();
+
+                        switch (food)
+                        {
+                            case 1:
+                                {
+                                    Food veggies = new Food(Food.FoodKind.VEGGIES);
+                                    cachito.EatVeggies(veggies);
+                                    //Console.ReadLine();
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    Food cheese = new Food(Food.FoodKind.CHEESE);
+                                    cachito.EatCheese(cheese);
+                                    Console.ReadLine();
+                                    break;
+                                }
+                            case 3:
+                                {
+                                    Food chocolate = new Food(Food.FoodKind.CHOCOLATE);
+                                    cachito.EatChocolate(chocolate);
+                                    Console.ReadLine();
+                                    break;
+                                }
+                            case 4:
+                                {
+                                    Food meat = new Food(Food.FoodKind.MEAT);
+                                    cachito.EatMeat(meat);
+                                    break;
+                                }
+
+                        } //Switch Kind of Food
+                        SaveInfo();
+                        break;
+                    } //Switch option Feed
+
+                case 2: //Play
+                    {
+                        cachito.Play();
+                        Console.ReadLine();
+
+                        /////////// RESTAR LA CANTIDAD DE CALORIAS GASTADAS  
+
+                        cachito.StopPlaying();
+                        Console.ReadLine();
+                        SaveInfo();
+                        break;
+                    }
+
+                case 3: //Walk
+                    {
+                        cachito.Walk();
+                        Console.ReadLine();
+                        SaveInfo();
+                        break;
+                    }
+
+                case 4: //Sleep
+                    {
+                        cachito.Sleep();
+                        Console.ReadLine();
+                        SaveInfo();
+                        break;
+                    }
+                case 5: //Exit
+                    {
+                        //SaveInfo();
+                        Console.Clear();
+                        Console.WriteLine("See you later!");
+                        Console.ReadLine();
+                        System.Environment.Exit(1);
+                        break;
+
+                    }
+
+                default:
+                    {
+                        Console.WriteLine("Sorry. This option is invalid. Try again");
+                        displayMenu();
+                        break;
+                    }
+            }
+            Console.Clear();
+            displayMenu();
+        }
+
+        public static void SaveInfo()
+        {
+            try
+            {
+                // Create a FileStream that will write data to file.
+                Stream myStream = File.Create(DATA_FILENAME);
+                BinaryFormatter formatter = new BinaryFormatter();
+                // Save current dog object created
+                formatter.Serialize(myStream, cachito);
+                myStream.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unable to save Tamagotchi's information");
+                Console.Write(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+        public static void LoadInfo()
+        {
+
+            // Check if there is already information of a Tamagotchi
+            if (File.Exists(DATA_FILENAME))
+            {
+                try
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    // Create a FileStream will gain read access to the data file.
+                    FileStream readerFileStream = new FileStream(DATA_FILENAME, FileMode.Open, FileAccess.Read);
+                    // Reconstruct information of our friends from file.
+                    cachito = (Dog)formatter.Deserialize(readerFileStream);
+                    // Close the readerFileStream when we are done
+                    readerFileStream.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("There seems to be a file that contains TamagotchiPractice's information but somehow there is a problem with reading it.");
+                    Console.Write(ex.Message);
+                    Console.ReadLine();
+                } // end try-catch
+
+            } 
+
+        } // end LoadInfo()
+
+        static void displayInfo()
+        {
+            Console.WriteLine("Current Total Calories: " + cachito.m_totalKalories);
+
         }
     }
 }
+
+
+
